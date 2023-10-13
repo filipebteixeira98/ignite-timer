8e38a285362cbfcf001a9c066ae54b5d74e04477
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-10-13 16:13:31 -0300

    chore: created global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
 
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>ignite timer</h1>
       <Button variant="secondary" />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
